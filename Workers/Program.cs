78d5db2e54295c3a71365e805b4a878be09d50b2
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-10-31 14:33:03 -0300

    Initial commit

---
+++ Workers/Program.cs
@@ -0,0 +1,10 @@
+using Workers;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
