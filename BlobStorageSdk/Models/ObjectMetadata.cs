0d67d602350ba4ee6c7161e7e193aabdc50b1b41
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-10-31 17:33:42 -0300

    Addition of default values to ObjectMetadata

--- BlobStorageSdk/Models/ObjectMetadata.cs
+++ BlobStorageSdk/Models/ObjectMetadata.cs
@@ -4,15 +4,20 @@ using System.Text.Json.Serialization;
 public class ObjectMetadata
 {
     [JsonPropertyName("name")]
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
+
     [JsonPropertyName("type")]
-    public string Type { get; set; }
+    public string Type { get; set; } = "";
+
     [JsonPropertyName("size")]
     public long Size { get; set; }
+
     [JsonPropertyName("last_modified")]
     public DateTime LastModified { get; set; }
+
     [JsonPropertyName("created_at")]
     public DateTime CreatedAt { get; set; }
+
     [JsonPropertyName("path")]
-    public string Path { get; set; }
+    public string Path { get; set; } = "";
 }
