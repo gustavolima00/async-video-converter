279de9ac031e70e693178682b923a6483146a766
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-01 13:48:02 -0300

    Addition of helper functions

--- Api/Controllers/FileConversionController.cs
+++ Api/Controllers/FileConversionController.cs
@@ -16,18 +16,23 @@ public class FileConversionController : ControllerBase
     }
 
     [HttpPost("send-file")]
-    public async Task<IActionResult> SendFileToConversion(IFormFile file)
+    public async Task<IActionResult> SendFileToConversion(IFormFile file, [FromQuery] string fileName)
     {
         if (file == null || file.Length == 0)
         {
             return BadRequest("Arquivo não enviado ou está vazio.");
         }
 
+        if (string.IsNullOrEmpty(fileName))
+        {
+            return BadRequest("Nome do arquivo não fornecido.");
+        }
+
         try
         {
             using (var stream = file.OpenReadStream())
             {
-                await _fileStorageService.SaveFileToConvertAsync(stream, file.FileName);
+                await _fileStorageService.SaveFileToConvertAsync(stream, fileName);
             }
 
             return Ok("Arquivo recebido e está na fila de conversão.");
--- BlobStorageSdk/BlobStorageApi.cs
+++ BlobStorageSdk/BlobStorageApi.cs
@@ -30,37 +30,77 @@ public class BlobStorageApi : IBlobStorageApi
         _baseApiUrl = configuration.BlobStorageUrl;
     }
 
-    private string BuildUrl(string path, Dictionary<string, string> queryParameters)
+    private string BuildUrl(string path, Dictionary<string, string>? queryParameters)
     {
         var builder = new UriBuilder($"{_baseApiUrl}{path}");
         var query = HttpUtility.ParseQueryString(builder.Query);
 
-        foreach (var (key, value) in queryParameters)
+        if (queryParameters != null)
         {
-            query[key] = value;
+            foreach (var (key, value) in queryParameters)
+            {
+                query[key] = value;
+            }
         }
 
         builder.Query = query.ToString();
         return builder.ToString();
     }
 
-    public async Task<List<ObjectMetadata>> ListFilesAndFoldersAsync(string pathPrefix)
+    private async Task<HttpContent> GetAsync(string path, Dictionary<string, string>? queryParameters = null)
     {
-        var requestUri = BuildUrl("/list", new()
-        {
-            ["path_prefix"] = pathPrefix
-        });
-
+        var requestUri = BuildUrl(path, queryParameters);
         var response = await _httpClient.GetAsync(requestUri);
 
+        if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+        {
+            var errorResponse = await response.Content.ReadAsStringAsync();
+            throw new BlobStorageApiException($"Erro na requisição: {errorResponse}");
+        }
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new BlobStorageApiException($"Falha ao listar arquivos e pastas. Status code: {response.StatusCode}");
+        }
+
+        return response.Content;
+    }
+
+    private async Task<Response> GetAndDeserializeAsync<Response>(string path, Dictionary<string, string>? queryParameters = null)
+    {
+        var response = await GetAsync(path, queryParameters);
+        var responseStream = await response.ReadAsStreamAsync();
+        return await JsonSerializer.DeserializeAsync<Response>(responseStream) ?? throw new BlobStorageApiException($"Falha ao desserializar resposta");
+    }
+
+    private async Task<Response> PostAndDeserializeAsync<Response>(string path, HttpContent? content = null, Dictionary<string, string>? queryParameters = null)
+    {
+
+        var requestUri = BuildUrl(path, queryParameters);
+        var response = await _httpClient.PostAsync(requestUri, content);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+        {
+            var errorResponse = await response.Content.ReadAsStringAsync();
+            throw new BlobStorageApiException($"Erro na requisição: {errorResponse}");
+        }
         if (!response.IsSuccessStatusCode)
         {
             throw new BlobStorageApiException($"Falha ao listar arquivos e pastas. Status code: {response.StatusCode}");
         }
 
         var responseStream = await response.Content.ReadAsStreamAsync();
-        var result = await JsonSerializer.DeserializeAsync<List<ObjectMetadata>>(responseStream) ?? throw new BlobStorageApiException($"Falha ao desserializar resposta. Status code: {response.StatusCode}");
-        return result;
+        return await JsonSerializer.DeserializeAsync<Response>(responseStream) ?? throw new BlobStorageApiException($"Falha ao desserializar resposta");
+    }
+
+    public async Task<List<ObjectMetadata>> ListFilesAndFoldersAsync(string pathPrefix)
+    {
+        return await GetAndDeserializeAsync<List<ObjectMetadata>>(
+            "/list",
+            new()
+            {
+                ["path_prefix"] = pathPrefix
+            }
+        );
     }
 
     public async Task<ObjectMetadata> UploadFileAsync(Stream fileStream, string fileName, string destinationPath)
@@ -69,38 +109,20 @@ public class BlobStorageApi : IBlobStorageApi
         {
             { new StreamContent(fileStream), "file", fileName }
         };
-        var requestUri = BuildUrl("/upload", new()
+
+        return await PostAndDeserializeAsync<ObjectMetadata>("/upload", content, new()
         {
             ["folder_path"] = destinationPath,
             ["file_name"] = fileName
         });
-
-        var response = await _httpClient.PostAsync(requestUri, content);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new BlobStorageApiException($"Falha ao fazer upload do arquivo. Status code: {response.StatusCode}");
-        }
-
-        var responseStream = await response.Content.ReadAsStreamAsync();
-        var result = await JsonSerializer.DeserializeAsync<ObjectMetadata>(responseStream) ?? throw new BlobStorageApiException($"Falha ao desserializar resposta. Status code: {response.StatusCode}");
-        return result;
     }
 
     public async Task<Stream> GetFileAsync(string filePath)
     {
-        var requestUri = BuildUrl("/get-file", new()
+        var response = await GetAsync("/get-file", new()
         {
             ["file_path"] = filePath
         });
-
-        var response = await _httpClient.GetAsync(requestUri);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new BlobStorageApiException($"Falha ao fazer download do arquivo. Status code: {response.StatusCode}");
-        }
-
-        return await response.Content.ReadAsStreamAsync();
+        return await response.ReadAsStreamAsync();
     }
 }
