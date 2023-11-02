e2575cb99a52a51f833ff01561f45240ef742204
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-02 15:30:01 -0300

    Fixing migration

--- migrations/Scripts/0001 - Init.sql
+++ migrations/Scripts/0001 - Init.sql
@@ -2,7 +2,5 @@ create table raw_file(
     id serial primary key,
     name varchar(255) not null,
     path varchar(255) not null,
-    metadata jsonb null,
+    metadata jsonb null
 );
-
-create index raw_file on raw_file(path);
