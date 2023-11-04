4fd1a15fe10c00927bfecf6b5180ca8b69a6232c
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-04 20:10:33 -0300

    Addition of more repository unit tests

--- src/Tests/Repositories/RawFileRepositoryTests.cs
+++ src/Tests/Repositories/RawFileRepositoryTests.cs
@@ -2,6 +2,7 @@ using Xunit;
 using System.Threading.Tasks;
 using Repositories;
 using Repositories.Postgres;
+using Repositories.Models;
 namespace Tests.Repositories;
 
 public class RawFilesRepositoriesTests
@@ -14,9 +15,8 @@ public class RawFilesRepositoriesTests
         {
             UseInMemoryDatabase = true
         };
-        var databaseConnection = new DatabaseConnection(postgresConfiguration);
         _context = new DatabaseContext(postgresConfiguration);
-        _repository = new RawFilesRepository(databaseConnection, _context);
+        _repository = new RawFilesRepository(_context);
     }
 
     [Fact]
@@ -24,5 +24,44 @@ public class RawFilesRepositoriesTests
     {
         var rawFile = await _repository.TryGetByIdAsync(1);
         Assert.Null(rawFile);
+    }
+
+    [Fact]
+    public async Task TryGetByIdAsyncReturnsRawFileWhenRawFileWithIdExists()
+    {
+        var rawFile = new RawFile
+        {
+            Id = 1,
+            Name = "test",
+            Path = "test",
+            ConversionStatus = ConversionStatus.NotConverted
+        };
+        _context.RawFiles.Add(rawFile);
+        await _context.SaveChangesAsync();
+        var rawFileFromRepository = await _repository.TryGetByIdAsync(1);
+        Assert.Equal(rawFile, rawFileFromRepository);
+    }
+
+    [Fact]
+    public async Task TryGetByPathAsyncReturnsNullWhenNoRawFileWithPathExists()
+    {
+        var rawFile = await _repository.TryGetByPathAsync("test");
+        Assert.Null(rawFile);
+    }
+
+    [Fact]
+    public async Task TryGetByPathAsyncReturnsRawFileWhenRawFileWithPathExists()
+    {
+        var rawFile = new RawFile
+        {
+            Id = 1,
+            Name = "test",
+            Path = "test",
+            ConversionStatus = ConversionStatus.NotConverted
+        };
+        _context.RawFiles.Add(rawFile);
+        await _context.SaveChangesAsync();
+        var rawFileFromRepository = await _repository.TryGetByPathAsync("test");
+        Assert.Equal(rawFile, rawFileFromRepository);
     }
 }
