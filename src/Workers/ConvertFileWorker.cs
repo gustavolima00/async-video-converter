13c36077313d38676051e67ae3efa8ea57fca3ab
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-05 17:50:19 -0300

    Updating conversion worker

--- src/Workers/ConvertFileWorker.cs
+++ src/Workers/ConvertFileWorker.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Repositories.Models;
 using Services;
 using Services.Configuration;
 
@@ -7,20 +8,34 @@ namespace Workers;
 public class ConvertFileWorker : BaseQueueWorker<int>
 {
     private readonly IRawFilesService _fileStorageService;
+    private readonly IWebVideoService _webVideoService;
     readonly string _queueUrl;
     public ConvertFileWorker(
         ILogger<ConvertFileWorker> logger,
         IQueueService queueService,
         IRawFilesService fileStorageService,
-        QueuesConfiguration queuesConfiguration
+        QueuesConfiguration queuesConfiguration,
+        IWebVideoService webVideoService
     ) : base(logger, queueService)
     {
         _fileStorageService = fileStorageService;
         _queueUrl = queuesConfiguration.ConvertQueueName;
+        _webVideoService = webVideoService;
     }
     protected override string QueueUrl => _queueUrl;
     protected override async Task ProcessMessage(int id, CancellationToken cancellationToken)
     {
-        await _fileStorageService.ConvertFileToMp4(id, cancellationToken);
+        try
+        {
+            await _fileStorageService.UpdateConversionStatus(id, ConversionStatus.Converting, cancellationToken);
+            var stream = await _fileStorageService.ConvertToMp4(id, cancellationToken);
+            await _webVideoService.SaveWebVideoAsync(stream, id, cancellationToken);
+            await _fileStorageService.UpdateConversionStatus(id, ConversionStatus.Converted, cancellationToken);
+        }
+        catch
+        {
+            await _fileStorageService.UpdateConversionStatus(id, ConversionStatus.Error, cancellationToken);
+            throw;
+        }
     }
 }
