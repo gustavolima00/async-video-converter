44320f2f4b09465dd045d921835657c05c40efab
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-06 18:58:18 -0300

    Separating converted videos and subtitles files

--- src/Repositories/Models/ConvertedSubtitle.cs
+++ src/Repositories/Models/ConvertedSubtitle.cs
@@ -3,28 +3,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Repositories.Models;
 
-[Table("converted_videos")]
-public class ConvertedVideo
+[Table("converted_subtitles")]
+public class ConvertedSubtitle
 {
+
     [Column("id")]
     public int Id { get; set; }
 
-    [Column("name")]
-    public string Name { get; set; } = "";
+    [Column("converted_video_id")]
+    public int ConvertedVideoId { get; set; }
 
     [Column("path")]
     public string Path { get; set; } = "";
 
+    [Column("language")]
+    public string Language { get; set; } = "";
+
     [Column("link")]
     public string Link { get; set; } = "";
 
-    [Column("raw_video_id")]
-
-    public int RawVideoId { get; set; }
-
     [Column("metadata")]
-
     public MediaMetadata? Metadata { get; set; }
-
-    public ICollection<ConvertedSubtitle> Subtitles { get; set; } = new List<ConvertedSubtitle>();
 }
